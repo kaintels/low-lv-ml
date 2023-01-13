593797b6fd3f2ac615b9b41ca6f936590e050c23
Seungwoo Han <seungwoohan0108@gmail.com>
2023-01-13 20:04:44 +0900

    Add Linear layer

---
+++ layer.h
@@ -0,0 +1,39 @@
+#include <Eigen/Core>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <string>
+
+Eigen::MatrixXd Linear(std::string layer, Eigen::MatrixXd input, int feature, int mini_batch){
+
+    float number;
+
+    std::ifstream awfile(layer + ".weight.txt");
+    std::ifstream abfile(layer + ".bias.txt");
+
+    auto weight = Eigen::MatrixXd(mini_batch, feature);
+    auto bias = Eigen::VectorXd(mini_batch);
+
+    for (int j = 0; j < weight.rows(); ++j){
+        for (int i = 0; i < weight.cols(); ++i) {
+            awfile >> number;
+            weight(j, i) = number;
+        }
+    }
+
+    for (int i = 0; i < bias.size(); ++i) {
+        abfile >> number;
+        bias(i) = number;
+    }
+    
+    auto dot_product = input.lazyProduct(weight.transpose());
+
+
+    auto result = Eigen::MatrixXd(dot_product.rows(), dot_product.cols());
+
+    for (int j = 0; j < dot_product.rows(); ++j){
+        result(j, Eigen::all) = dot_product(j, Eigen::all).transpose() + bias;
+    }
+
+    return result;
+}
