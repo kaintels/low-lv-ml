8bb51b15fe7e7adeb635654e9076de945f7fc08f
Seungwoo Han <seungwoohan0108@gmail.com>
2023-01-14 16:33:26 +0900

    Add Tanh, ReLU, LeakyReLU

--- activation.h
+++ activation.h
@@ -7,10 +7,21 @@
 
 
 Eigen::MatrixXd Sigmoid(Eigen::MatrixXd input){
+    Eigen::MatrixXd result = 1.0 / (1.0 + (-input).array().exp());
+    return result;
+}
 
-    auto m_input = -input;
-    auto exp_ = m_input.array().exp();
-    Eigen::MatrixXd result = 1.0 / (1.0 + exp_);
+Eigen::MatrixXd Tanh(Eigen::MatrixXd input){
+    Eigen::MatrixXd result = (input.array().exp() - (-input).array().exp()) / (input.array().exp() + (-input).array().exp());
+    return result;
+}
 
+Eigen::MatrixXd ReLU(Eigen::MatrixXd input){
+    Eigen::MatrixXd result = input.array().cwiseMax(0);
+    return result;
+}
+
+Eigen::MatrixXd LeakyReLU(Eigen::MatrixXd input, float negative_slope){
+    Eigen::MatrixXd result = input.array().cwiseMax(0) + negative_slope * input.array().cwiseMin(0);
     return result;
 }
