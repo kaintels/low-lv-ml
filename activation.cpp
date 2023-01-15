fc34b5c50a1dccf9273aff88c6706c4f69155619
Seungwoo Han <seungwoohan0108@gmail.com>
2023-01-16 01:14:21 +0900

    Add cpp files

---
+++ activation.cpp
@@ -0,0 +1,27 @@
+#include "activation.h"
+
+
+Eigen::MatrixXd Sigmoid(Eigen::MatrixXd input){
+    Eigen::MatrixXd result = 1.0 / (1.0 + (-input).array().exp());
+    return result;
+}
+
+Eigen::MatrixXd Tanh(Eigen::MatrixXd input){
+    Eigen::MatrixXd result = (input.array().exp() - (-input).array().exp()) / (input.array().exp() + (-input).array().exp());
+    return result;
+}
+
+Eigen::MatrixXd ReLU(Eigen::MatrixXd input){
+    Eigen::MatrixXd result = input.array().cwiseMax(0);
+    return result;
+}
+
+Eigen::MatrixXd LeakyReLU(Eigen::MatrixXd input, float negative_slope){
+    Eigen::MatrixXd result = input.array().cwiseMax(0) + negative_slope * input.array().cwiseMin(0);
+    return result;
+}
+
+Eigen::MatrixXd ELU(Eigen::MatrixXd input, float alpha){
+    Eigen::MatrixXd result = input.array().cwiseMax(0) + alpha * (input.array().cwiseMin(0).exp() - 1);
+    return result;
+}
