2591f7a750be7c94d6814d71f9cdac98d0a9cf09
Seungwoo Han <seungwoohan0108@gmail.com>
2023-01-16 01:14:38 +0900

    Update header files

--- activation.h
+++ activation.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <cmath>
 #include <Eigen/Core>
 #include <fstream>
@@ -5,28 +6,12 @@
 #include <ostream>
 #include <string>
 
+Eigen::MatrixXd Sigmoid(Eigen::MatrixXd input);
 
-Eigen::MatrixXd Sigmoid(Eigen::MatrixXd input){
-    Eigen::MatrixXd result = 1.0 / (1.0 + (-input).array().exp());
-    return result;
-}
+Eigen::MatrixXd Tanh(Eigen::MatrixXd input);
 
-Eigen::MatrixXd Tanh(Eigen::MatrixXd input){
-    Eigen::MatrixXd result = (input.array().exp() - (-input).array().exp()) / (input.array().exp() + (-input).array().exp());
-    return result;
-}
+Eigen::MatrixXd ReLU(Eigen::MatrixXd input);
 
-Eigen::MatrixXd ReLU(Eigen::MatrixXd input){
-    Eigen::MatrixXd result = input.array().cwiseMax(0);
-    return result;
-}
+Eigen::MatrixXd LeakyReLU(Eigen::MatrixXd input, float negative_slope);
 
-Eigen::MatrixXd LeakyReLU(Eigen::MatrixXd input, float negative_slope){
-    Eigen::MatrixXd result = input.array().cwiseMax(0) + negative_slope * input.array().cwiseMin(0);
-    return result;
-}
-
-Eigen::MatrixXd ELU(Eigen::MatrixXd input, float alpha){
-    Eigen::MatrixXd result = input.array().cwiseMax(0) + alpha * (input.array().cwiseMin(0).exp() - 1);
-    return result;
-}
+Eigen::MatrixXd ELU(Eigen::MatrixXd input, float alpha);
--- layer.h
+++ layer.h
@@ -1,51 +1,10 @@
+#pragma once
 #include <Eigen/Core>
 #include <fstream>
 #include <iostream>
 #include <ostream>
 #include <string>
 
-Eigen::MatrixXd Flatten(Eigen::MatrixXd input, std::string major="row"){
+Eigen::MatrixXd Flatten(Eigen::MatrixXd input, std::string major);
 
-    if (major == "row"){
-        auto result = input.reshaped<Eigen::RowMajor>().transpose();
-        return result;
-    }
-
-    else {
-        auto result = input.reshaped().transpose();
-        return result;
-    }
-}
-
-Eigen::MatrixXd Linear(std::string layer_name, Eigen::MatrixXd input, int feature, int mini_batch){
-
-    float number;
-
-    std::ifstream awfile(layer_name + ".weight.txt");
-    std::ifstream abfile(layer_name + ".bias.txt");
-
-    auto weight = Eigen::MatrixXd(mini_batch, feature);
-    auto bias = Eigen::VectorXd(mini_batch);
-
-    for (int j = 0; j < weight.rows(); ++j){
-        for (int i = 0; i < weight.cols(); ++i) {
-            awfile >> number;
-            weight(j, i) = number;
-        }
-    }
-
-    for (int i = 0; i < bias.size(); ++i) {
-        abfile >> number;
-        bias(i) = number;
-    }
-    
-    auto dot_product = input.lazyProduct(weight.transpose());
-    auto result = Eigen::MatrixXd(dot_product.rows(), dot_product.cols());
-
-    for (int j = 0; j < dot_product.rows(); ++j){
-        result(j, Eigen::all) = dot_product(j, Eigen::all).transpose() + bias;
-    }
-
-    return result;
-}
-
+Eigen::MatrixXd Linear(std::string layer_name, Eigen::MatrixXd input, int feature, int mini_batch);
